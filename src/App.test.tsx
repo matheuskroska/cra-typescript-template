ab7ece2a1123a5aa3cedc8ea0203a299d6bb87fd
Matheus <kroskamatheus@gmail.com>
2022-11-05 01:42:49 -0300

    Initial Commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
 
 test('renders learn react link', () => {
-  const { getByText } = render(
+  render(
     <Provider store={store}>
       <App />
     </Provider>
@@ -13,3 +12,5 @@ test('renders learn react link', () => {
 
   expect(screen.getByText(/learn/i)).toBeInTheDocument()
 })
+
+// Path: src\index.tsx
